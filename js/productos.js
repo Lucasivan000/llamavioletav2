628cfb99f3ff453a304d8af715663ee639051071
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-04-17 12:58:17 -0300

    primero comite

---
+++ js/productos.js
@@ -0,0 +1,125 @@
+
+const products = [
+    {
+        id: 1,
+        name: "Cartera Alba",
+        subtitle: "Hecha a mano en macramé. Personalizable.",
+        images: [
+            "img/alba-1.jpg",
+            "img/alba-2.jpg",
+            "img/alba-3.jpg"
+        ],
+        historia:"j ",
+        details: "Cada cartera se teje a mano sin moldes industriales.",
+        personalization: "Podés elegir colores y detalles.",
+        times: "Los tiempos varían según el modelo."
+    },
+
+    {
+        id: 2,
+        historia:"i ",
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"h ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+     {
+        id: 3,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"g ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+     {
+        id: 4,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"f ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+     {
+        id: 5,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"e ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+     {
+        id: 6,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"d ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+     {
+        id: 7,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:" c",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+    {
+        id: 8,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"b ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    },
+    {
+        id: 9,
+        name: "Cartera Lúa",
+        subtitle: "Textura suave, diseño liviano.",
+        images: [
+            "img/lua-1.jpg",
+            "img/lua-2.jpg"
+        ],
+        historia:"a ",
+        details: "Macramé artesanal con variaciones únicas.",
+        personalization: "Diseño adaptable a pedido.",
+        times: "Producción artesanal personalizada."
+    }
+
+
+];
